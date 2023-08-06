ddd026f44df7a1b9c3ecefe84c976bb19e26ae76
Adwithya Magow <avmagow@gmail.com>
2023-08-07 00:35:12 +0800

    feat(rget): parse url from command line

--- rget/src/main.rs
+++ rget/src/main.rs
@@ -3,17 +3,12 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(short, long)]
-    name: String,
-
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+    #[arg(required = true, help = "url to download")]
+    url: String,
 }
 
 fn main() {
     let args = Args::parse();
 
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name)
-    }
+    println!("{}", args.url)
 }
