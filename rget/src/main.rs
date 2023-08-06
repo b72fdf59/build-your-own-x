311fa802506f78092afaff0fb6ef591cf58961ab
Adwithya Magow <avmagow@gmail.com>
2023-08-07 01:06:49 +0800

    feat(rget): write progress bar function

--- rget/src/main.rs
+++ rget/src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use indicatif::{ProgressBar, ProgressStyle};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -11,4 +12,30 @@ fn main() {
     let args = Args::parse();
 
     println!("{}", args.url)
+}
+
+fn create_progress_bar(
+    msg: &'static str,
+    length: Option<u64>,
+    quite_mode: Option<bool>,
+) -> ProgressBar {
+    let bar = match quite_mode {
+        Some(true) => ProgressBar::hidden(),
+        Some(false) | None => match length {
+            Some(len) => ProgressBar::new(len),
+            None => ProgressBar::new_spinner(),
+        },
+    };
+
+    bar.set_message(msg);
+    let style = match length.is_some() {
+        true => ProgressStyle::default_bar()
+                .progress_chars("=> ")
+                .template("{msg} {spinner:green} [{elapsed_precise}] [{wide_bar:cyan/blue}] {bytes}/{total_bytes} eta:{eta}")
+                .unwrap(),
+        false => ProgressStyle::default_spinner(),
+    };
+    bar.set_style(style);
+
+    bar
 }
---
+++ rget/target/debug/deps/rget-728b99e350000597.d
@@ -0,0 +1,5 @@
+/home/amagow/Documents/build-your-own-x/rget/target/debug/deps/rget-728b99e350000597.rmeta: src/main.rs
+
+/home/amagow/Documents/build-your-own-x/rget/target/debug/deps/rget-728b99e350000597.d: src/main.rs
+
+src/main.rs:
